f2d84486a7a35b01e9e0d070ccec3211f7ecd377
Alina Mihai <mihai@synyx.de>
2022-08-24 20:50:51 +0200

    according to python coding conventions: using snakes instead of camels

--- app.py
+++ app.py
@@ -3,20 +3,23 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 moods = []
 
+
 @app.route("/")
 def hello_world():
     return render_template('index.html')
 
 
 @app.route("/setMood")
-def setMood():
+def set_mood():
     mood = request.args.get("mood")
     moods.append(mood)
     return "<p>Stimmung gespeichert!</p>" + mood
 
 
 @app.route("/getMood")
-def getMood():
-    moodString = ", ".join(str(mood) for mood in moods)
+def get_mood():
+    mood_string = ", ".join(str(mood) for mood in moods)
 
-    return "<p>Alle Stimmungen, die gespeichert wurden:<p>" + moodString
+    return "<p>Alle Stimmungen, die gespeichert wurden:<p>" + mood_string
+
+
