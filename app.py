eafb881592e87e4415b0b03e6ef838fef8da815c
Petra Scherer <scherer@synyx.de>
2022-08-24 20:28:17 +0200

    Platz schaffen und dritte Seite getMood hinzufügen

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+moods = []
 
 @app.route("/")
 def hello_world():
     return render_template('index.html')
+
+
 @app.route("/setMood")
 def setMood():
-
     mood = request.args.get("mood")
-
-
+    moods.append(mood)
     return "<p>Stimmung gespeichert!</p>" + mood
+
+
+@app.route("/getMood")
+def getMood():
+    moodString = ", ".join(str(mood) for mood in moods)
+
+    return "<p>Alle Stimmungen, die gespeichert wurden:<p>" + moodString
